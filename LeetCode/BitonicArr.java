638d3149f0e2fce037497d7f9d07cb0ee858b36b
Sanidhya1912 <sanidhyac1912@gmail.com>
2024-06-29 00:29:53 +0530

    .

--- LeetCode/BitonicArr.java
+++ LeetCode/BitonicArr.java
@@ -13,18 +13,16 @@ public class BitonicArr {
         int start = 0;
         int end = arr.length - 1;
 
-        while (start <= end) {
+        while (start < end) {
             int mid = start + (end - start) / 2;
 
             if (arr[mid] < arr[mid + 1]) {
                 start = mid + 1;
-            } else if (arr[mid] > arr[mid + 1]) {
-                end = mid - 1;
             } else {
-                return mid;
+                end = mid;
             }
         }
 
-        return -1;
+        return start;
     }
 }
