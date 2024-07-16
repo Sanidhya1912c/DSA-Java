7054668729413f68d52f319cf99040feb8dc1025
Sanidhya1912 <sanidhyac1912@gmail.com>
2024-07-16 22:53:32 +0530

    cyclic sort is also done, its kinda feels easy and lame to me ngl

--- Algorithms/Shorting/InsertionSort.java
+++ Algorithms/Shorting/InsertionSort.java
@@ -11,17 +11,15 @@ public class InsertionSort {
 
     static void insertionSort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = i + 1; j < arr.length;) {
-                if (j == 0)
-                    break;
-
+            for (int j = i + 1; j > 0; j--) {
                 if (arr[j] < arr[j - 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j - 1];
                     arr[j - 1] = temp;
 
+                } else {
+                    break;
                 }
-                j--;
 
             }
         }
