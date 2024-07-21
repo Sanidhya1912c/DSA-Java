ccf55d78a3ad0f2d02d4bd21812f5c2a6e7891c2
Sanidhya1912 <sanidhyac1912@gmail.com>
2024-07-21 09:09:11 +0530

    .

--- CourseQuestions/CyclicSort/FindDuplicate.java
+++ CourseQuestions/CyclicSort/FindDuplicate.java
@@ -4,17 +4,17 @@ import java.util.ArrayList;
 
 public class FindDuplicate {
     public static void main(String[] args) {
-        int[] arr = { 1, 3, 4, 2, 2, 2, 4, 4, 3, 3 };
+        int[] nums = { 1, 3, 4, 2, 2, 2, 4, 4, 3, 3 };
 
         int i = 0;
-        while (i < arr.length) {
-            int correct = arr[i] - 1;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
 
-            if (arr[i] != arr[correct]) {
+            if (nums[i] != nums[correct]) {
 
-                int temp = arr[i];
-                arr[i] = arr[correct];
-                arr[correct] = temp;
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
             } else {
                 i++;
             }
@@ -23,11 +23,11 @@ public class FindDuplicate {
         ArrayList<Integer> dublicates = new ArrayList<Integer>();
 
         int lastDublicate = 0;
-        for (int j = 0; j < arr.length; j++) {
-            if (arr[j] != j + 1) {
-                if (lastDublicate != arr[j]) {
-                    dublicates.add(arr[j]);
-                    lastDublicate = arr[j];
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j + 1) {
+                if (lastDublicate != nums[j]) {
+                    dublicates.add(nums[j]);
+                    lastDublicate = nums[j];
                 }
             }
         }
